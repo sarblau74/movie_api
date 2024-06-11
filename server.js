f8c3ead46a1771a9ff648fd7be4eb9dd78a6ab70
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-06-11 09:04:49 +0200

    added users to server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const express = require("express"),
-  morgan = require('morgan'),
-  fs = require('fs'),
-  path = require('path');
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-let movies = [
+let topMovies = [
     {
         id: 1,
         title: "The Godfather",
@@ -88,31 +86,50 @@ let movies = [
         }
 ];
 
-// Create a write stream (in append mode)
-const accesLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), { flags: "a" });
+// Dummy user data for demonstration
+let users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'jane.doe@example.com'
+    }
+];
+// Express static function
+app.use(express.static('public'));
 
-// Setup the logger
-app.use(morgan("combined", { stream: accesLogStream }));
+// Middleware
+app.use(morgan('common'));
 
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.send("Welcome To Movie API ")
+// GET requests
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
 });
 
-app.get("/movies", (req, res) => {
-  res.json(movies)
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-app.get("/documentation", (req, res) => {
-  res.senFile("public/documentation.html")
-})
-
-app.use((err, req, res, next) => { 
-  console.error(err.stack);
-  res.status(500).send("Something is broken!")
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
 });
 
+// Define the /users route
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Listen for requests
 app.listen(8080, () => {
-  console.log("Server is running on port 8080")
+    console.log('Your app is listening on port 8080.');
 });
