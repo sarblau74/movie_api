512eb056dfae47a5861baa33ec7247d6c46f725f
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-07-10 17:30:40 +0200

    created auth.js

---
+++ auth.js
@@ -0,0 +1,31 @@
+const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+require('./passport'); // Your local passport file
+
+module.exports = (app) => {
+  app.use(passport.initialize());
+
+  app.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        const token = jwt.sign(user.toJSON(), jwtSecret, {
+          subject: user.Username, // This is the username you’re encoding in the JWT
+          expiresIn: '7d', // This specifies that the token will expire in 7 days
+          algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
+        });
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+};
